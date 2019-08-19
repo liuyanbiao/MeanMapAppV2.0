a45f2da31bfaa9e69fa624664de39b5191394e87
root <root@EITLAP257.localdomain>
2019-08-19 14:18:13 +1200

    adding all

--- app/config.js
+++ app/config.js
@@ -1,26 +1,10 @@
-// Sets the MongoDB Database options
-
 module.exports = {
 
-    mongolab:
-    {
-        name: "mongolab",
-        url: "mongodb://BigCoder:BigCoder!@ds139817.mlab.com:39817/heroku_cnkn5vpn",
-        port: 27017
-    },
+	    bitnami:
+	    {
+		            name: "MongoDB Service",
+		            url: "mongodb://" + process.env.DATABASE_USERNAME + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
+		            port: process.env.DATABASE_PORT
+		        }
 
-    local:
-    {
-        name: "scotch-user-map-local",
-        url: "mongodb://localhost/MeanMapApp",
-        port: 27017
-    },
-
-    localtest:
-    {
-        name: "scotch-user-map-local",
-        url: "mongodb://localhost/MeanMapAppTest",
-        port: 27017
-    }
-
-};
+}
